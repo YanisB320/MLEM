88197832eaea348981b75551a06c5ba8839f2bd3
Yanis Brihi <yanis.brihi@epita.fr>
2021-12-06 23:49:48 +0100

    :coffin: remove dead code

--- train_producer.py
+++ train_producer.py
@@ -11,8 +11,6 @@ producer = KafkaProducer(
 
 df_train = pd.read_csv('data/train_preprocessed.csv')
 df_train = df_train.dropna()
-# df_test = pd.read_csv('data/test_preprocessed.csv')
-# df_test = df_test.dropna()
 
 X = df_train.iloc[:,1:]
 y = df_train.iloc[:,0]
@@ -23,11 +21,7 @@ for _, row in X.iterrows():
         'X' : [row.tolist()],
         'y' : int(y.iloc[i])
         }
-    # data_test = {
-    #     'X' : [row.tolist()]
-    #     }
     producer.send('train', value=data_train)
-    # producer.send('train', value=data_test)
 
     i += 1
 
