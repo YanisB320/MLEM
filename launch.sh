68f3b602c9133c282b8ee51a3ebb9749be4180b0
Yanis Brihi <yanis.brihi@epita.fr>
2021-12-06 23:46:38 +0100

    :green_heart: fix launch.sh

--- launch.sh
+++ launch.sh
@@ -12,14 +12,13 @@ cd kafka_2.13-3.0.0
 # start kafka environment
 bin/zookeeper-server-start.sh config/zookeeper.properties > /dev/null &
 bin/kafka-server-start.sh config/server.properties > /dev/null &
-bin/kafka-topics.sh --create --topic test --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 > /dev/null
 bin/kafka-topics.sh --create --topic train --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 > /dev/null
 
 cd ..
 
 python train_producer.py &
 python test_producer.py &
-python train_consumer.py
+python consumer.py
 
 # terminate kafka environment
 echo 'terminate kafka environment'
